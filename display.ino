c720e8c1216ad9da5bb8ba5fadbd3c288f8c4eeb
Adrián Falb <4drian.falb@gmail.com>
2024-03-22 11:47:45 +0100

    Write code for top bar, side menu bar, main GPS screen

---
+++ display.ino
@@ -0,0 +1,509 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#define TFT_CS        10
+#define TFT_RST        8
+#define TFT_DC         9
+#define TFT_MOSI      11
+#define TFT_SCLK      13
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+float p = 3.1415926;
+
+void updateTopBar() {
+  tft.setCursor(68, 2);
+  tft.setTextColor(ST77XX_WHITE); 
+  tft.setTextSize(1);
+
+  int numOfSatellites = 3; // tu by som bral hodnotu z nejakej get funkcie
+  tft.print(numOfSatellites);
+
+  tft.setCursor(tft.width() - 24, 2);
+  int boatBattery = 50; // tu by som bral hodnotu z nejakej get funkcie
+  tft.print(boatBattery);
+  tft.print("%");
+
+  tft.setCursor(68, 10);
+
+  bool connected = true; // tu by som bral hodnotu z nejakej get funkcie
+  if (connected) {
+    tft.print("OK");
+  } else {
+    tft.print("NO");
+  }
+
+  tft.setCursor(tft.width() - 24, 10);
+  int joystickBattery = 100; // tu by som bral hodnotu z nejakej get funkcie
+  tft.print(joystickBattery);
+  tft.print("%");
+}
+
+void drawTopBar() {
+  // Mohol by som to zbuchat aj tak, ze v tejto draw funkcii vykreslim vsetko co je staticke
+  // a potom v oddelenej update funkcii by som len aktualizoval premenlive hodnoty
+  // kazda scena by teda mohla mat taketo dve funkcie, to by sa dalo.
+
+  int rectX = 0;
+  int rectY = 0;
+  int rectWidth = tft.width();
+  int rectHeight = 20; // Adjust the height of the rectangle as needed
+  tft.fillRect(rectX, rectY, rectWidth, rectHeight, ST77XX_RED);
+
+  // Satellites
+  tft.setCursor(0, 2);
+  tft.setTextColor(ST77XX_WHITE);  tft.setTextSize(1);
+  tft.print("Satellites: ");
+
+  // Boat Battery
+  tft.setCursor(tft.width() - 56, 2);
+  tft.print("Boat: ");
+  tft.println();
+  
+  // Connection status
+  tft.print("Connection: ");
+
+  // Joystick battery
+  tft.setCursor(tft.width() - 80, 10);
+  tft.print("Joystick: "); 
+}
+
+void drawRightMenuBar() {
+
+  // Menu bar
+  int rectX = tft.width() - 80;
+  int rectY = 20;
+  int rectWidth = tft.width();
+  int rectHeight = tft.height();
+  tft.fillRect(rectX, rectY, rectWidth, rectHeight, ST77XX_BLUE);
+
+  // Buttons with text
+  int buttonWidth  = 60;
+  int buttonHeight = 30;
+
+  // Menu button
+  tft.fillRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_BLACK);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("MENU");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.fillRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_BLACK);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("SELECT");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.fillRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_BLACK);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("CONFIRM");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.fillRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_BLACK);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("HOME");
+  tft.setCursor(rectX + 14, rectY + 24 + 8);
+  //tft.print("POSITION");
+  rectY = rectY + 20 + buttonHeight;  
+}
+
+void drawMainScreenBackground() {  
+  // Main screen
+  int rectX = 0;
+  int rectY = 20;
+  int rectWidth = tft.width() - 80;
+  int rectHeight = tft.height() - 20;
+  //tft.fillRect(rectX, rectY, rectWidth, rectHeight, ST77XX_BLACK);
+  tft.drawRect(rectX, rectY, rectWidth, rectHeight, ST77XX_WHITE);
+}
+
+void updateMainScreenGpsValues() {
+  const int CURSOR_NEW_LINE = 10;
+
+  int rectX = 20;
+  int rectY = 20;
+  int rectWidth = tft.width(); - 80;
+  int rectHeight = tft.height();
+
+  int buttonWidth  = 180;
+  int buttonHeight = 30;
+   
+  tft.setTextSize(1); 
+  tft.setTextColor(ST77XX_WHITE);
+
+  // GPS Position 1
+  tft.setCursor(rectX + 14, rectY + 24 + CURSOR_NEW_LINE);
+
+  tft.print("Lat: ");
+  float latitude = 40.780; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(latitude);
+
+  tft.print(", Long: ");
+  float longitude = 73.562;
+  tft.print(longitude); // tuto by som bral hodnotu z nejakej get funkcie
+  rectY = rectY + 20 + buttonHeight;
+
+  // GPS Position 2
+  tft.setCursor(rectX + 14, rectY + 24 + CURSOR_NEW_LINE);
+
+  tft.print("Lat: ");
+  latitude = 40.780; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(latitude);
+
+  tft.print(", Long: ");
+  longitude = 73.562; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(longitude);
+  rectY = rectY + 20 + buttonHeight;
+
+  // GPS Position 3
+  tft.setCursor(rectX + 14, rectY + 24 + CURSOR_NEW_LINE);
+
+  tft.print("Lat: ");
+  latitude = 40.780; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(latitude);
+
+  tft.print(", Long: ");
+  longitude = -73.562; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(longitude);
+
+  rectY = rectY + 20 + buttonHeight;
+
+  // GPS Position 4
+  tft.setCursor(rectX + 14, rectY + 24 + CURSOR_NEW_LINE);  
+
+  tft.print("Lat: ");
+  latitude = 40.780; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(latitude);
+
+  tft.print(", Long: ");
+  longitude = -73.562; // tuto by som bral hodnotu z nejakej get funkcie
+  tft.print(longitude);
+
+  rectY = rectY + 20 + buttonHeight;
+}
+
+void drawMainScreenGps() {
+  drawMainScreenBackground();
+  
+  const int CURSOR_NEW_LINE = 10;
+
+  // Menu bar
+  int rectX = 20;
+  int rectY = 20;
+  int rectWidth = tft.width(); - 80;
+  int rectHeight = tft.height();
+
+  // Buttons with text
+  int buttonWidth  = 180;
+  int buttonHeight = 30;
+
+  // Menu button
+  tft.drawRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_WHITE);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("GPS POSITION 1");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.drawRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_WHITE);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("GPS POSITION 2");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.drawRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_WHITE);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("GPS POSITION 3");
+  rectY = rectY + 20 + buttonHeight;
+
+  tft.drawRect(rectX + 10, rectY + 20, buttonWidth, buttonHeight, ST77XX_WHITE);
+  tft.setCursor(rectX + 14, rectY + 24);
+  tft.setTextSize(1); tft.setTextColor(ST77XX_WHITE);
+  tft.print("GPS POSITION 4");
+  rectY = rectY + 20 + buttonHeight;
+}
+
+unsigned long updateDisplay() {
+  // Update values
+  updateTopBar();
+  updateMainScreenGpsValues();
+
+  // Write logic for redrawing layout when button is pressed
+  // ...
+}
+
+void initDisplay() {
+  // Use this initializer (uncomment) if using a 2.0" ST7789 320x240 TFT:
+  tft.init(240, 320);
+  tft.setRotation(1); // aby bola 0,0 v lavom hornom rohu
+  tft.invertDisplay(false); // for some reason the default of the display is to be inverted...
+  tft.fillScreen(ST77XX_BLACK);
+
+  drawTopBar();
+  drawRightMenuBar();
+  drawMainScreenGps();
+}
+
+void setup(void) {
+  //Serial.begin(9600);
+  //Serial.print(F("Hello! ST77xx TFT Test"));
+
+  initDisplay();  
+}
+
+void loop() {
+  updateDisplay();
+}
+
+void testDisplay() {
+  // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
+  // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
+  // may end up with a black screen some times, or all the time.
+  //tft.setSPISpeed(40000000);
+
+  Serial.println(F("Initialized"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+
+  // large block of text
+  tft.fillScreen(ST77XX_BLACK);
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+  delay(1000);
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+
+  // a single pixel
+  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
+  delay(500);
+
+  // line draw test
+  testlines(ST77XX_YELLOW);
+  delay(500);
+
+  // optimized lines
+  testfastlines(ST77XX_RED, ST77XX_BLUE);
+  delay(500);
+
+  testdrawrects(ST77XX_GREEN);
+  delay(500);
+
+  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
+  delay(500);
+
+  tft.fillScreen(ST77XX_BLACK);
+  testfillcircles(10, ST77XX_BLUE);
+  testdrawcircles(10, ST77XX_WHITE);
+  delay(500);
+
+  testroundrects();
+  delay(500);
+
+  testtriangles();
+  delay(500);
+
+  mediabuttons();
+  delay(500);
+
+  Serial.println("done");
+  delay(1000);
+}
+
+void testlines(uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, 0, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, 0, 0, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+    delay(0);
+  }
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void testfastlines(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t y=0; y < tft.height(); y+=5) {
+    tft.drawFastHLine(0, y, tft.width(), color1);
+  }
+  for (int16_t x=0; x < tft.width(); x+=5) {
+    tft.drawFastVLine(x, 0, tft.height(), color2);
+  }
+}
+
+void testdrawrects(uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+  }
+}
+
+void testfillrects(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=tft.width()-1; x > 6; x-=6) {
+    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+  }
+}
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
+    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
+    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testtriangles() {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 0xF800;
+  int t;
+  int w = tft.width()/2;
+  int x = tft.height()-1;
+  int y = 0;
+  int z = tft.width();
+  for(t = 0 ; t <= 15; t++) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x-=4;
+    y+=4;
+    z-=4;
+    color+=100;
+  }
+}
+
+void testroundrects() {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 100;
+  int i;
+  int t;
+  for(t = 0 ; t <= 4; t+=1) {
+    int x = 0;
+    int y = 0;
+    int w = tft.width()-2;
+    int h = tft.height()-2;
+    for(i = 0 ; i <= 16; i+=1) {
+      tft.drawRoundRect(x, y, w, h, 5, color);
+      x+=2;
+      y+=3;
+      w-=4;
+      h-=6;
+      color+=1100;
+    }
+    color+=100;
+  }
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons() {
+  // play
+  tft.fillScreen(ST77XX_BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
+}
+
